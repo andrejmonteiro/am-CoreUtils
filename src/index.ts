89d14c3fe9a3e66cb76d06d3c7060e98917cecec
André Monteiro <4868121+andrejmonteiro@users.noreply.github.com>
2024-05-11 05:53:56 +0100

    changed how the modules are exported to clean imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 /** @format */
 
-export * from "./logger";
+/**
+ * Core module
+ * @module logger
+ * @description This module is responsible for logging the messages to the console
+ */
+import logger from "./logger";
+
+/**
+ * Exports all the modules from the core to be used in the application
+ */
+export { logger };
